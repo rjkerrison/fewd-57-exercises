8a5e259fa756c40e809f5cd218c92c2e83505ed6
Thai Truong <sontruong@hotmail.co.uk>
2020-10-31 23:30:27 +0000

    Javascript added
    Javascript added to display modal and remove modal.

---
+++ homework/trulia/thai/app.js
@@ -0,0 +1,15 @@
+const modalBtn = document.querySelector(".modal-btn");
+const modalBg = document.querySelector(".modal-bg");
+const modalClose = document.querySelector(".modal-close");
+
+modalBtn.addEventListener("click", function () {
+  modalBg.classList.add("bg-active");
+});
+modalClose.addEventListener("click", function () {
+  modalBg.classList.remove("bg-active");
+});
+
+// attempt to apply for loops for document.querySelectorAll so it applies to all modal buttons
+// for (i = 0; i < 13; i+=) {
+//   modalBtn;
+// }
