a17ecc77918e592abf8e542ec8a848a899a00eea
Thai Truong <sontruong@hotmail.co.uk>
2020-11-01 16:50:34 +0000

    JS code added to hide modal by clicking outside

--- homework/trulia/thai/app.js
+++ homework/trulia/thai/app.js
@@ -9,6 +9,14 @@ modalClose.addEventListener("click", function () {
   modalBg.classList.remove("bg-active");
 });
 
+window.addEventListener("click", function (event) {
+  if (event.target == modalBg) {
+    modalBg.classList.remove("bg-active");
+  }
+});
+
+// modalBg.classList.remove("bg-active");
+
 // attempt to apply for loops for document.querySelectorAll so it applies to all modal buttons
 // for (i = 0; i < 13; i+=) {
 //   modalBtn;
