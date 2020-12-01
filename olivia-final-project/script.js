e6df0f6e26cec3ed2d2c349db14c3c7fe4aacf08
oliviasherwood333 <72411648+oliviasherwood333@users.noreply.github.com>
2020-12-01 20:39:09 +0000

    changes

---
+++ olivia-final-project/script.js
@@ -0,0 +1,31 @@
+const menuButton = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuButton.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuButton.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuButton.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+
+
+
+
+// grab a reference to all the elements that will control the show/hide of the nav
+const navIcons = document.querySelectorAll('.menu-btn.js-nav-icon');
+
+// loop through each of the nav icon triggers and add an event listener
+navIcons.forEach(function (icon) {
+	icon.addEventListener('click', toggleNav);
+});
+
+// create the event handler function to toggle the active state of the site nav
+function toggleNav(event) {
+	const siteNav = document.querySelector('.user-nav-open');
+	siteNav.classList.toggle('is-active');
+}
+
+
