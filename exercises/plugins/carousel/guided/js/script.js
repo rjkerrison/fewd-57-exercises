2d13dcc92713243cef99f2a278d5bd8e5b67c6f2
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-12-10 12:02:45 +0000

    guided carousel

---
+++ exercises/plugins/carousel/guided/js/script.js
@@ -0,0 +1,13 @@
+// grab a reference to all the elements that will control the show/hide of the nav
+const navIcons = document.querySelectorAll('.js-nav-icon');
+
+// loop through each of the nav icon triggers and add an event listener
+navIcons.forEach(function (icon) {
+	icon.addEventListener('click', toggleNav);
+});
+
+// create the event handler function to toggle the active state of the site nav
+function toggleNav(event) {
+	const siteNav = document.querySelector('.site-nav');
+	siteNav.classList.toggle('is-active');
+}
